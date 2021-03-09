2153c5db7ef739077af0d0da8ecaf412e0b92818
Bogdan Luca <luca.bogdan@gmail.com>
2021-03-09 13:12:52 +0000

    Add exercise 1 solution.

--- src/exercise1/index.js
+++ src/exercise1/index.js
@@ -5,3 +5,30 @@
 // methods" section.
 // Pay attention to error handling and exit codes
 // Make it support a single file, for now.
+const fs = require('fs');
+const crypto = require('crypto');
+
+function md5Hash(filename) {
+  const hash = crypto.createHash('md5');
+  const data = fs.readFileSync(filename);
+  return hash.update(data).digest('hex');
+}
+
+if (process.argv.length !== 3) {
+  console.error('Error: Invalid number of arguments');
+  console.log('Usage: node index.js FILE');
+  process.exit(1);
+}
+
+const filename = process.argv[2];
+
+// console.log('running folder:', process.cwd());
+
+try {
+  const hash = md5Hash(filename);
+  console.log(`${hash}  ${filename}`);
+} catch (err) {
+  console.error(`node index.js: ${filename}: ${err.message}`);
+  // console.error('error stacktrace:', err.stack);
+  process.exit(1);
+}
