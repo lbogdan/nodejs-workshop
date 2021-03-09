4d46c943ce09c20f2e4eb5c4a327bbd8f2278dd1
Bogdan Luca <luca.bogdan@gmail.com>
2021-03-09 08:39:01 +0200

    Update exercise.

--- src/exercise10/index.js
+++ src/exercise10/index.js
@@ -2,3 +2,16 @@
 // Use the pi-generator package and the worker_threads core module
 // to offload the processing to a separate thread.
 // Check what happens when calling it multiple times in parallel.
+const pi = require('pi-generator');
+
+function syncPINthDigit(n) {
+  let env;
+
+  for (let i = 0; i < n + 1; i++) {
+    const digitEnv = pi.nextDigit(env);
+    if (i === n) {
+      return digitEnv[0];
+    }
+    env = digitEnv[1];
+  }
+}
